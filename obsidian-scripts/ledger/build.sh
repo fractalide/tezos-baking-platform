69edbdd0b08aa211922e2cf88cd33e97dd3e02a4
Ryan Trinkle <ryan.trinkle@gmail.com>
2018-04-23 17:38:04 -0400

    Add scripts to build and install ledger app

---
+++ obsidian-scripts/ledger/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+#TODO: Fully nixify this build
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd "$DIR"
+
+export BOLOS_ENV="$(nix-build --no-out-link bolos-env.nix)"
+export BOLOS_SDK="$PWD/nanos-secure-sdk"
+
+"$(nix-build --no-out-link fhs.nix)/bin/enter-fhs" <<EOF
+cd blue-app-ssh-agent
+make
+EOF
