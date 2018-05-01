c026660ca957767a5eb20c679e5558cc47d7d346
Cale Gibbard <cgibbard@gmail.com>
2018-05-01 01:05:50 -0400

    Report the amount of tezzies in the baking account. Be a little smarter about how we invoke the client.

--- obsidian-scripts/monitored-bakers.sh
+++ obsidian-scripts/monitored-bakers.sh
@@ -2,7 +2,6 @@
 
 for i in $(seq 1 "$#") ; do
     echo "LAUNCH BAKER:" "${!i}"
-    tezos-bake-monitor --port $((9800 + i)) --rpchost 127.0.0.1:$((18731 + i)) -- `which tezos-sandbox-client.sh` --port $((18731 + i)) launch daemon "${!i}" -B -E -D &
+    tezos-bake-monitor --port $((9800 + i)) --rpchost 127.0.0.1:$((18731 + i)) --client `which tezos-sandbox-client.sh` --identity "${!i}" &
+    sleep 1
 done
-
-
