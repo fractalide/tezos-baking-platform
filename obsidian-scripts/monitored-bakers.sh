eb970134a91319ea6394fb2a815f13f5743b83f6
jhartzell <jimmy.hartzell@obsidian.systems>
2018-05-02 14:32:07 -0400

    this is now generated by a script

--- obsidian-scripts/monitored-bakers.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-for i in $(seq 1 "$#") ; do
-    echo "LAUNCH BAKER:" "${!i}"
-    tezos-bake-monitor --port $((9800 + i)) --rpchost 127.0.0.1:$((18731 + i)) --client `which tezos-sandbox-client.sh` --identity "${!i}" &
-    sleep 1
-done
