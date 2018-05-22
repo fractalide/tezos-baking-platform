d9f7d1b6c5fb003d4b59e21bbe17846b1aa823aa
Jimmy Hartzell <jimmy.hartzell@obsidian.systems>
2018-05-22 11:10:28 -0400

    verify individual keys

--- ledger/sign.sh
+++ ledger/sign.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
 
+if [ "X${2:-}" != "X" ]; then
+    byte=$2
+else
+    byte=c1
+fi
 {
-    echo 8004000009028000002c800006c1
+    echo 8004000009028000002c800006$byte
     echo -n 8004810081
-    if [ "${1:-}X" = X ]; then
-        cat
-    else
-        echo "$1"
-    fi
+    echo "$1"
 } | nix-shell ledger-blue-shell.nix --pure --run 'python -m ledgerblue.runScript --apdu'
