468bd070db9589e65cc86b18d0a7500d8a0e64a0
Jimmy Hartzell <jimmy.hartzell@obsidian.systems>
2018-05-29 16:46:38 -0400

    build both baking and combined apps; address.sh modified to line up with sign.sh for baking app

--- ledger/address.sh
+++ ledger/address.sh
@@ -5,5 +5,5 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd "$DIR"
 
 {
-    echo 8002000011048000002c800006c18000000080000000
+    echo 8002000011028000002c800006c1
 } | nix-shell ledger-blue-shell.nix --pure --run 'python -m ledgerblue.runScript --apdu'
--- ledger/build.sh
+++ ledger/build.sh
@@ -9,8 +9,16 @@ export BOLOS_ENV="$(nix-build --no-out-link bolos-env.nix)"
 export BOLOS_SDK="$PWD/nanos-secure-sdk"
 
 "$(nix-build --no-out-link fhs.nix)/bin/enter-fhs" <<EOF
+set -eux
 cd ledger-app
 export BOLOS_SDK=$BOLOS_SDK
 export BOLOS_ENV=$BOLOS_ENV
-make $@
+make clean
+make
+mv bin/app.hex generic.hex
+mv bin/app.elf generic.elf
+export BAKING_APP=Y
+make clean
+make
+mv generic.* bin/
 EOF
