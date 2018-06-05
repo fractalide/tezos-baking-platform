70c21c7314474a7cddf0b9f04baa4f18bd2b9f4a
Jimmy Hartzell <jimmy.hartzell@obsidian.systems>
2018-06-05 13:52:47 -0400

    install both apps from single script

--- ledger/install-nix.sh
+++ ledger/install-nix.sh
@@ -3,8 +3,7 @@ set -eu
 
 cd "$(dirname "$0")"
 
-export app_hex="$1"
-export app_name="$2"
+export app_name="$1"
 
 #TODO: Pin nixpkgs
-nix-shell ledger-blue-shell.nix --run 'set -x; ./install.sh "'"$app_hex"'" "'"$app_name"'"'
+nix-shell ledger-blue-shell.nix --run 'set -x; ./ledger-app/install.sh "'"$app_name"'"'
