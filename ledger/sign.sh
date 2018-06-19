21c1a1705272ddaecb4e550bfd9e6a81dd8ab78f
Jimmy Hartzell <jimmy.hartzell@obsidian.systems>
2018-06-19 14:01:40 -0400

    update scripts to stricter enforcement of APDU lengths

--- ledger/sign.sh
+++ ledger/sign.sh
@@ -4,13 +4,8 @@ set -euo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
 
-if [ "X${2:-}" != "X" ]; then
-    byte=$2
-else
-    byte=c1
-fi
 {
-    echo 8004000009028000002c800006$byte
-    echo -n 8004810081
+    echo 8004000009${3:-02}8000002c800006c1${2:-}
+    printf "80048100%02x" "$(expr $(echo -n "$1" | wc -c) / 2)"
     echo "$1"
 } | nix-shell ledger-blue-shell.nix --pure --run 'python -m ledgerblue.runScript --apdu'
