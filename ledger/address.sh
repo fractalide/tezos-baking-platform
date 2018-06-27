961c6a0497ce2a9d200fe3cd2a2b07798aeed2fc
Jimmy Hartzell <jimmy.hartzell@obsidian.systems>
2018-06-27 18:41:02 -0400

    better commit for tezos

--- ledger/address.sh
+++ ledger/address.sh
@@ -5,5 +5,5 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd "$DIR"
 
 {
-    echo 8002000009028000002c800006c1
+    echo 8003000209028000002c800006c1
 } | nix-shell ledger-blue-shell.nix --pure --run 'python -m ledgerblue.runScript --apdu'
