ab6ca11a82d8febc3d59c138a73f40548d44d912
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-09-27 11:09:28 +0800

    submodules: ledger-app nanos-secure-sdk -> derivations
    Adapt setup_ledger.sh, replace build.sh with build.nix.

---
+++ nix/pins/opam2nix-packages/bump.sh
@@ -0,0 +1,29 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash -p bash gawk git nix-prefetch-git
+
+set -e
+set -u
+set -o pipefail
+
+baseurl=https://github.com
+repo=obsidiansystems/opam2nix-packages
+branch=refs/heads/master
+
+SCRIPT_NAME=${BASH_SOURCE[0]##*/}
+
+cd "${BASH_SOURCE[0]%$SCRIPT_NAME}"
+
+if (( $# > 1 )); then
+  echo "Usage: $SCRIPT_NAME [revision]"
+  exit 2
+fi
+
+if (( $# == 1 )); then
+  rev=$1
+else
+  rev=$(git ls-remote $baseurl/$repo.git |
+          awk '$2 == "'"$branch"'" { print $1 }')
+fi
+
+nix-prefetch-git --no-deepClone $baseurl/$repo.git $rev > default.json.new
+mv default.json{.new,}
